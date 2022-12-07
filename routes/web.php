b82b1c695a6f0729cf77be8ef0d25bbfa4964c7a
Kevin Chandra <kacol.bot@gmail.com>
2022-12-07 17:16:33 +0700

    breeze

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Gamify\Points\PurchaseAmountReward;
-use App\Gamify\Points\RegistrationReward;
-use App\Gamify\Points\TransactionReward;
-use App\Models\Customer;
-use App\Models\Transaction;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,62 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('customer/{customer}', function (Customer $customer) {
-    //$transaction = new Transaction([
-    //    'amount' => 36000,
-    //]);
-    //
-    //$customer->givePoint(new TransactionReward($transaction));
-    //$customer->givePoint(new PurchaseAmountReward($transaction));
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    $customer->givePoint(new RegistrationReward($customer));
-
-    return $customer->reputations;
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::view('loyalty', 'loyalty');
-
-Route::get('loyalty', function () {
-    $data = [];
-
-    if (request()->has('member_id')) {
-        $memberId = request('member_id');
-        $memberId = str_replace('-', '', $memberId);
-        $data['customer'] = Customer::where('member_id', $memberId)->firstOrFail();
-    }
-
-    return view('loyalty', $data);
-})->name('loyalty');
-
-Route::post('loyalty', function (Request $request) {
-    $memberId = $request->member_id;
-    $amount = $request->amount;
-
-    $customer = Customer::where('member_id', $memberId)->firstOrFail();
-    $transaction = new Transaction();
-    $transaction->transaction_id = 'INV/20221108/000002';
-    $transaction->amount = $amount;
-
-    $customer->givePoint(new TransactionReward($transaction));
-    $customer->givePoint(new PurchaseAmountReward($transaction));
-
-    return redirect()->route('loyalty', ['member_id' => $customer->member_id]);
-});
-
-Route::middleware('auth')
-    ->get('check-points', function () {
-    $data = [];
-
-    if (request()->has('member_id')) {
-        $memberId = request('member_id');
-        $memberId = str_replace('-', '', $memberId);
-
-        $customer = Customer::query()
-                            ->where('member_id', $memberId)
-                            ->orWhere('phone', $memberId)
-                            ->firstOrFail();
-        $data['customer'] = $customer;
-    }
-
-    return view('check_points', $data);
-})->name('check_points');
+require __DIR__.'/auth.php';
